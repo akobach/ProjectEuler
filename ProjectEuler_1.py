08b33daa013f7ddbdb8a995b1f6e8158c6ef9fd4
Andrew Kobach <45322435+akobach@users.noreply.github.com>
2022-10-18 19:51:16 -0700

    Update ProjectEuler_1.py
    Bug fixed

--- ProjectEuler_1.py
+++ ProjectEuler_1.py
@@ -8,17 +8,31 @@ The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
+
 def sum_first_n_ints(k):
     return k*(k+1)/2
 
 
 def solution(a, b, n):
+    # kj*j is the largest multiple of j _less than or equal to_ n
+    # here, j = a, b, or a*b
     ka = (n - (n % a)) / a
     kb = (n - (n % b)) / b
     kab = (n - (n % (a*b))) / (a*b)
+
+    # the problem asks for largest multiples of a or b _strictly_ less than n
+    # correct for this
+    if n % a == 0:
+        ka -= 1
+    if n % b == 0:
+        kb -= 1
+    if n % (a*b) == 0:
+        kab -= 1
+
+    # use analytic formula for sum
+    # only works for two input multiples (a and b)
     result = int(a*sum_first_n_ints(ka) + b*sum_first_n_ints(kb) - a*b*sum_first_n_ints(kab))
-    if n % a == 0 or n % b == 0:
-        result -= n
+
     return result
 
 
@@ -30,3 +44,6 @@ if __name__ == "__main__":
     # this solution is analytic
     # only works for two multiples
     print(solution(3, 5, n))
+
+# extra challenge
+# extend this algorithm to analytically solve for the sum for an arbitrary number of multiples
