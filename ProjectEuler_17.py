a4829e5d5f6b7147ad205ec5148f8168417508a2
Andrew Kobach <45322435+akobach@users.noreply.github.com>
2022-11-02 17:40:40 -0700

    Create ProjectEuler_17.py

---
+++ ProjectEuler_17.py
@@ -0,0 +1,28 @@
+"""
+Project Euler problem #17
+by Andrew Kobach
+Date: 2 Nov 2022
+
+If the numbers 1 to 5 are written out in words:
+one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words,
+how many letters would be used?
+
+NOTE: Do not count spaces or hyphens.
+For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen)
+contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+"""
+
+from num2words import num2words
+
+if __name__ == "__main__":
+    result = 0
+    for n in range(1, 1001):
+        words = num2words(n)
+        words = words.replace("-", " ")
+        words = "".join(words.split())
+        result += len(words)
+        
+    print(result)
+
